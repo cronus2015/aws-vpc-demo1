6499a163da04f871417a3811bc6b0f8669facfc8
cronus2015 <114941059+cronus2015@users.noreply.github.com>
2026-04-24 21:23:36 -0600

    Update internetgateway.tf

--- internetgateway.tf
+++ internetgateway.tf
@@ -1,10 +1,9 @@
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.mycustomvpc.id
   tags = {
-    "contct" = "Emmanuel Sedas"
-    "app"    = "Emmanuel'VPC-demo"
+    "contct" = "owner"
+    "app"    = "owner's VPC"
     "env"    = "test"
-    "elcid"  = "itclarch"
     Name = "IGW"
   }
 }
